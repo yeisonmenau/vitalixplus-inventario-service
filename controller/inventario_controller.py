07bb8b6168041638850a48ff887ccb293bfe8993
yeisonmenau <yeisonandresdh@gmail.com>
2025-11-17 15:25:27 -0500

    Crear inventario controlador

---
+++ controller/inventario_controller.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from service.inventario_service import InventarioService
+
+router = APIRouter(prefix="/inventario", tags=["Inventario"])
+
+servicio = InventarioService()
+
+@router.get("/")
+def listar():
+    return servicio.listar_todo()
+
+@router.get("/codigo/{item_id}")
+def buscar_por_id(item_id: int):
+    return servicio.buscar_por_id(item_id)
+
+@router.get("/nombre/{nombre}")
+def buscar_por_nombre(nombre: str):
+    return servicio.buscar_por_nombre(nombre)
